0339caa824a4959baa9bd800f066f1823cbc5827
am-thomas <annmthomas1999@gmail.com>
2025-08-25 11:56:45 -0500

    add path to metadata folder

--- src/constants.py
+++ src/constants.py
@@ -8,6 +8,7 @@ from pathlib import Path
 # Directory paths
 PARENT_PATH = Path(__file__).parent
 DATA_PATH = PARENT_PATH / '../data'
+METADATA_PATH = PARENT_PATH / '../metadata'
 RAW24H_PATH = DATA_PATH / 'raw-seismic'
 DETECTIONS_PATH = DATA_PATH / 'detections'
 PICKLISTS_PATH = DATA_PATH / 'pick-lists'
